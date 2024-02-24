00fa8c510677086f68d7333485af27bf21840906
gnouh404 <090297huong@gmail.com>
2024-02-24 22:42:15 +0700

    24/2 bổ sung validate-email + validate-username

--- env/expenses_website/authentication/views.py
+++ env/expenses_website/authentication/views.py
@@ -1,6 +1,33 @@
+
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+import json
+from django.contrib.auth.models import User
+from validate_email import validate_email
 # Create your views here.
+# validate_email chua dung
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email ko hop le'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'email đã tồn tại'}, status=409)
+        return JsonResponse({'email_valid':True})  
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username chỉ chứa chữ và số'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'username đã tồn tại'}, status=409)
+        return JsonResponse({'username_valid':True})    
+            
 class RegistrationView(View):
     def get(self, request):
-        return render(request, 'authentication/register.html')
+        return render(request, 'authentication/register.html')    
---
+++ env/expenses_website/expenses_website/static/js/register.js
@@ -0,0 +1,63 @@
+
+const usernameField = document.querySelector('#usernameField');
+const emailField = document.querySelector('#emailField');
+const feedbackArea = document.querySelector('.invalid_feedback');
+const emailFeedbackArea = document.querySelector('.emailFeedbackArea');
+const usernameSuccessOutput = document.querySelector('.usernameSuccessOutput');
+
+// username validate
+usernameField.addEventListener('keyup',(e) =>{
+    
+    const usernameVal = e.target.value;
+    // hieu ung check username xanh
+    usernameSuccessOutput.style.display = "block";
+    usernameSuccessOutput.textContent =`Checking ${usernameVal}`;
+    
+    // console.log("usernameVal", usernameVal);
+
+    usernameField.classList.remove('is-invalid');
+    feedbackArea.style.display  =   'none';
+
+    if(usernameVal.length>0){
+        fetch("/authentication/validate-username", {
+            body : JSON.stringify({ username: usernameVal }),
+            method : "POST",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data" , data);
+            usernameSuccessOutput.style.display = "none";
+            if(data.username_error){
+                usernameField.classList.add('is-invalid');
+                feedbackArea.style.display  =   'block';
+                feedbackArea.innerHTML  =   `<p>${data.username_error}</p>`;
+            }
+        });
+    }
+})
+// email validate
+emailField.addEventListener('keyup',(e) =>{
+    // console.log("777777",777777);
+    const emailVal = e.target.value;
+    // console.log("emailVal", emailVal);
+
+    emailField.classList.remove('is-invalid');
+    emailFeedbackArea.style.display  =   'none';
+
+    if(emailVal.length>0){
+        fetch("/authentication/validate-email", {
+            body : JSON.stringify({ email: emailVal }),
+            method : "POST",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data" , data);
+            
+            if(data.email_error){
+                emailField.classList.add('is-invalid');
+                emailFeedbackArea.style.display  =   'block';
+                emailFeedbackArea.innerHTML  =   `<p>${data.email_error}</p>`;
+            }
+        });
+    }
+})
