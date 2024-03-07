37161404dd9153b718747dbd53aeab1069936922
gnouh404 <090297huong@gmail.com>
2024-03-07 21:08:44 +0700

    fix user's prefered currency

--- env/expenses_website/userpreferences/views.py
+++ env/expenses_website/userpreferences/views.py
@@ -6,32 +6,30 @@ from .models import UserPreference
 from django.contrib import messages
 # Create your views here.
 def index(request):
+    # tạo 1 list rỗng để lưu dictionary của các loại tiền tệ
+    currency_data = []
+    file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+        for k,v in data.items():
+            currency_data.append({'name':k,'value':v})
+    # 1 biến để ktra user có tồn tại ko         
     exists = UserPreference.objects.filter(user=request.user).exists()
+    # đặt đối tượng tiền tệ của user là none
     user_preferences = None
+    # Nếu tồn tại user thì gán đối tượng user_preferences cho user hiện tại
     if exists:
         user_preferences = UserPreference.objects.get(user=request.user)
-        import pdb
-        pdb.set_trace()
+    # Trả về phía ng dùng file index, bao gồm dữ liệu currencies là các loại tiền tệ để ng dùng chọn, user_preferences của người dùng    
     if request.method == "GET":
-        currency_data = []
-        file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
-        #debugger by pdb
-        # import pdb
-        # pdb.set_trace()
-        with open(file_path, 'r') as json_file:
-            data = json.load(json_file)
-            for k,v in data.items():
-                currency_data.append({'name':k,'value':v}) 
-        return render(request, 'preferences/index.html',{'currencies':currency_data})
+        return render(request, 'preferences/index.html',{'currencies':currency_data, 'user_preferences':user_preferences})
     else:
         currency = request.POST['currency']
         if exists:
-            import pdb
-            pdb.set_trace()
             user_preferences.currency = currency
             user_preferences.save()
         else:   
             UserPreference.objects.create(user=request.user, currency = currency)    
-            messages.success(request, "Changes saved")
-        return render(request, 'preferences/index.html', {'currency':currency_data})
+        messages.success(request, "Changes saved")
+        return render(request, 'preferences/index.html', {'currencies':currency_data, 'user_preferences':user_preferences})
          
