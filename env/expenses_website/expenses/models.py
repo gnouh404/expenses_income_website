d0652f9e5e13d45d8f73c6cee72b58698e33f3e3
gnouh404 <090297huong@gmail.com>
2024-03-07 23:32:15 +0700

    add expense for user

--- env/expenses_website/expenses/models.py
+++ env/expenses_website/expenses/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 # Create your models here.
+class Expense(models.Model):
+    amount = models.FloatField()
+    date = models.DateField(default = now)
+    description = models.TextField()
+    owner = models.ForeignKey(to=User, on_delete = models.CASCADE)
+    category = models.CharField(max_length = 255)
+    
+    def __str__(self) -> str:
+        return self.category
+    # model meta cua django giup thuc hien 1 so chuc nang
+    class Meta:
+        # sap xep giam dan theo ngay
+        ordering = ['-date']
+
+class Category(models.Model):
+    name = models.CharField(max_length = 255)
+    
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        # đổi tên
+        verbose_name_plural = 'Categories'        
--- env/expenses_website/expenses/views.py
+++ env/expenses_website/expenses/views.py
@@ -1,12 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
+from django.contrib import messages
 
 # Create your views here.
 
 # protect route, tránh việc đã log out ấn quay lại vẫn ở tài khoản chưa đăng xuất
 @login_required(login_url="/authentication/login")
 def index(request):
+    categories = Category.objects.all()
     return render(request, 'expenses/index.html')
 
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories':categories,
+        'values':request.POST
+    }
+    if request.method == "GET":
+        return render(request, 'expenses/add_expense.html', context)
+    # kiem soat dau vao cua ng dung
+    if request.method == "POST":
+        amount = request.POST['amount']
+        description = request.POST['description']
+        category = request.POST['category']
+        date = request.POST['expense_date']
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expense.html', context)
+        if not description:
+            messages.error(request, 'Description is required')
+            return render(request, 'expenses/add_expense.html', context)
+        if not date:
+            messages.error(request, 'Date is required')
+            return render(request, 'expenses/add_expense.html', context)  
+        Expense.objects.create(amount = amount, description = description, owner = request.user, date = date, category = category)
+        messages.success(request, 'Add expense successfully')
+        return redirect('expenses')
