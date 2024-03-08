404b1fd0525f4e4859bd5808fb7f93233a8c8690
gnouh404 <090297huong@gmail.com>
2024-03-08 23:16:33 +0700

    add and delete expense

--- env/expenses_website/expenses/urls.py
+++ env/expenses_website/expenses/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', views.index, name='expenses'),
     path('add-expense', views.add_expense, name = 'add-expense'),
-    
+    path('edit-expense/<int:id>', views.edit_expense, name ='edit-expense'),
+    path('delete-expense/<int:id>', views.delete_expense, name ='delete-expense'),
 ]
--- env/expenses_website/expenses/views.py
+++ env/expenses_website/expenses/views.py
@@ -9,7 +9,12 @@ from django.contrib import messages
 @login_required(login_url="/authentication/login")
 def index(request):
     categories = Category.objects.all()
-    return render(request, 'expenses/index.html')
+    expenses = Expense.objects.filter(owner = request.user)
+    context ={
+        'expenses':expenses,
+        'categories':categories,
+    }
+    return render(request, 'expenses/index.html',context)
 
 def add_expense(request):
     categories = Category.objects.all()
@@ -37,3 +42,41 @@ def add_expense(request):
         Expense.objects.create(amount = amount, description = description, owner = request.user, date = date, category = category)
         messages.success(request, 'Add expense successfully')
         return redirect('expenses')
+    
+def edit_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    categories = Category.objects.all()
+    context={
+        'expense':expense,
+        'values':expense,
+        'categories':categories
+    }
+    if request.method == "GET":
+        return render(request, 'expenses/edit-expense.html',context)
+    if request.method == "POST":
+        amount = request.POST['amount']
+        description = request.POST['description']
+        category = request.POST['category']
+        date = request.POST['expense_date']
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/edit-expense.html', context)
+        if not description:
+            messages.error(request, 'Description is required')
+            return render(request, 'expenses/edit-expense.html', context)
+        if not date:
+            messages.error(request, 'Date is required')
+            return render(request, 'expenses/edit-expense.html', context)  
+        expense.amount = amount 
+        expense.description = description  
+        expense.date = date 
+        expense.category = category
+        expense.save()
+        messages.success(request, 'Edit expense successfully')
+        return redirect('expenses')
+    
+def delete_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    expense.delete()      
+    messages.success(request, "Expense removed")
+    return redirect('expenses')     
