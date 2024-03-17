c67f5c90b864d5d612c8c07fe8caa17ac0b6a9e5
gnouh404 <090297huong@gmail.com>
2024-03-17 21:56:04 +0700

    add set-new-pass

--- env/expenses_website/authentication/urls.py
+++ env/expenses_website/authentication/urls.py
@@ -1,4 +1,4 @@
-from .views import RegistrationView,UsernameValidationView,EmailValidationView,VerificationView,LoginView,LogoutView
+from .views import RegistrationView,UsernameValidationView,EmailValidationView,VerificationView,LoginView,LogoutView,ResetPassword,CompletePasswordReset
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
     path('validate-email',csrf_exempt(EmailValidationView.as_view()), name='validate-email'),
     path('activate/<uidb64>/<token>', csrf_exempt(VerificationView.as_view()), name='activate'),
+    path('set-new-password/<uidb64>/<token>', csrf_exempt(CompletePasswordReset.as_view()), name='reset-user-password'),
     path('login', csrf_exempt(LoginView.as_view()), name='login'),
     path('logout', csrf_exempt(LogoutView.as_view()), name='logout'),
+    path('reset-password', csrf_exempt(ResetPassword.as_view()), name='reset-password'),
 ]
